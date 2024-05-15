f899641decc0b7ba5b00bad1941ffbd7cdfb068b
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-15 22:23:04 +0300

    Add object Cryptographer

---
+++ app/src/main/java/com/alongo/cryptonotcurrency/Cryptographer.kt
@@ -0,0 +1,27 @@
+package com.alongo.cryptonotcurrency
+
+import android.content.Context
+import androidx.security.app.authenticator.AppAuthenticator
+
+object Cryptographer {
+    fun verifyApp(context: Context, packageName: String): Boolean {
+        val result = AppAuthenticator.createFromResource(context, R.xml.app_public_key)
+            .checkAppIdentity("org.telegram.messenger")
+        return when (result) {
+            AppAuthenticator.SIGNATURE_MATCH -> {
+                println("Application has a valid signature")
+                true
+            }
+
+            AppAuthenticator.SIGNATURE_NO_MATCH -> {
+                println("Application has invalid signature or is not installed")
+                false
+            }
+
+            else -> {
+                println("Unknown AppAuthenticator result")
+                false
+            }
+        }
+    }
+}
