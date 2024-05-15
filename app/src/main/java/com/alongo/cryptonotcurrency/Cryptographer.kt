0fdd9395aaaff46c84d9d900ebb6499421b92ff0
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-15 22:24:30 +0300

    Replace hardcoded value with packageName

--- app/src/main/java/com/alongo/cryptonotcurrency/Cryptographer.kt
+++ app/src/main/java/com/alongo/cryptonotcurrency/Cryptographer.kt
@@ -6,7 +6,7 @@ import androidx.security.app.authenticator.AppAuthenticator
 object Cryptographer {
     fun verifyApp(context: Context, packageName: String): Boolean {
         val result = AppAuthenticator.createFromResource(context, R.xml.app_public_key)
-            .checkAppIdentity("org.telegram.messenger")
+            .checkAppIdentity(packageName)
         return when (result) {
             AppAuthenticator.SIGNATURE_MATCH -> {
                 println("Application has a valid signature")
