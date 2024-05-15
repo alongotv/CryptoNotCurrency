3ca5be6e98f92953ca450baf3fa7f21e4763da14
Vladimir Vetrov <pdastore1@gmail.com>
2024-05-15 23:15:01 +0300

    Remove greeting

--- app/src/main/java/com/alongo/cryptonotcurrency/MainActivity.kt
+++ app/src/main/java/com/alongo/cryptonotcurrency/MainActivity.kt
@@ -129,21 +129,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoNotCurrencyTheme {
-        Greeting("Android")
-    }
-}
-
 const val masterKeyAlias = "notcrypto"
 const val masterKeySizeBits = 256
